e929ee83de58cc63568168405d7a7a54f9527f2c
tomasod123 <tmsdwyer50@gmail.com>
2021-07-13 14:11:40 +0800

    initial commit with the old code from code sandbox. Still need to add gitignore

---
+++ src/QuoteList.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import QuoteData from "./quotes.json";
+
+class QuoteList extends Component {
+    render() {
+        return (
+            <div>
+                {QuoteData.map((quoteDetail, index) => {
+                    return (
+                        <div className="App Flexbox-container">
+                            <div className="TextComponent Flexbox-item-1">
+                                <h1>{quoteDetail.quote}</h1>
+                            </div>
+
+                            <div className="Source">
+                                <h4>
+                                    {quoteDetail.author}{" "}
+                                    <span className="Years">{quoteDetail.lifespan}</span>
+                                </h4>
+                            </div>
+
+                            <div className="TextComponent Flexbox-item-2">
+                                <button className="PlusButton">+</button>
+                                <span>{quoteDetail.votes}</span>
+                                <button className="MinusButton">-</button>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
+}
+
+export default QuoteList;
