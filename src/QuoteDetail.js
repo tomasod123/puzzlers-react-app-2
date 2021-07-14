b38a49040e676056b14ac44d32e5d9fbec8cc68e
tomasod123 <tmsdwyer50@gmail.com>
2021-07-14 14:26:09 +0800

    converted component to funcdtional component

--- src/QuoteDetail.js
+++ src/QuoteDetail.js
@@ -1,30 +1,59 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
-class QuoteDetail extends Component {
-    render() {
-        const { quote } = this.props;
-        return (
-            <div className="App Flexbox-container">
-                <div className="TextComponent Flexbox-item-1">
-                    <h1>{quote.quote}</h1>
-                </div>
 
-                <div className="Source">
-                    <h4>
-                        {quote.author}{" "}
-                        <span className="Years">{quote.lifespan}</span>
-                    </h4>
-                </div>
+const QuoteDetail = (quote) => {
+    return (
 
-                <div className="TextComponent Flexbox-item-2">
-                    <button className="PlusButton">+</button>
-                    {/* <span>{quote.votes}</span> */}
-                    <span>0</span>
-                    <button className="MinusButton">-</button>
-                </div>
+        <div className="App Flexbox-container">
+            <div className="TextComponent Flexbox-item-1">
+                <h1>{quote.quote}</h1>
             </div>
-        );
-    }
-}
+
+            <div className="Source">
+                <h4>
+                    {quote.author}{" "}
+                    <span className="Years">{quote.lifespan}</span>
+                </h4>
+            </div>
+
+            <div className="TextComponent Flexbox-item-2">
+                <button className="PlusButton">+</button>
+                {/* <span>{quote.votes}</span> */}
+                <span>0</span>
+                <button className="MinusButton">-</button>
+            </div>
+        </div>
+    );
+};
+
+
+
+// class QuoteDetail extends Component {
+
+//     render() {
+//         const { quote } = this.props;
+//         return (
+//             <div className="App Flexbox-container">
+//                 <div className="TextComponent Flexbox-item-1">
+//                     <h1>{quote.quote}</h1>
+//                 </div>
+
+//                 <div className="Source">
+//                     <h4>
+//                         {quote.author}{" "}
+//                         <span className="Years">{quote.lifespan}</span>
+//                     </h4>
+//                 </div>
+
+//                 <div className="TextComponent Flexbox-item-2">
+//                     <button className="PlusButton">+</button>
+//                     {/* <span>{quote.votes}</span> */}
+//                     <span>0</span>
+//                     <button className="MinusButton">-</button>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
 
 export default QuoteDetail;
