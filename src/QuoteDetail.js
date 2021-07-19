30810bd1d1836dc89479db376ccd2f30391cb363
tomasod123 <tmsdwyer50@gmail.com>
2021-07-19 21:05:42 +0800

    style updates

--- src/QuoteDetail.js
+++ src/QuoteDetail.js
@@ -3,21 +3,23 @@ import React, { useState } from "react";
 
 const QuoteDetail = (props) => {
     const { quote, name } = props
+    // Using props to give the initial count, before incrementing or decrementing 
     const [count, setCount] = useState(quote.votes);
 
+    //State is handled within the component and is updated within the component, while props are handled outside of the component and must be changed outside of the component
+    //Changing the state in a component will rerender that component
     function decrementCount() {
-        setCount(prevCount => prevCount - 1)
-    }
 
+        if (count >= 0) { setCount(prevCount => prevCount - 1); }
+
+    }
     function incrementCount() {
         setCount(prevCount => prevCount + 1)
     }
-
     return (
-
         <div className="App Flexbox-container">
             <div className="TextComponent Flexbox-item-1">
-                <h1>{quote.quote}</h1>
+                <h1 className="quoteText">"{quote.quote}"</h1>
             </div>
 
             <div className="Source">
@@ -30,41 +32,11 @@ const QuoteDetail = (props) => {
             <div className="TextComponent Flexbox-item-2">
                 <button className="PlusButton" onClick={incrementCount}>+</button>
                 {/* <span>{quote.votes}</span> */}
-                <span>{count}</span>
+                <span className="count">{count}</span>
                 <button className="MinusButton" onClick={decrementCount}>-</button>
             </div>
         </div>
     );
 };
-
-
-
-// class QuoteDetail extends Component {
-
-//     render() {
-//         const { quote } = this.props;
-//         return (
-//             <div className="App Flexbox-container">
-//                 <div className="TextComponent Flexbox-item-1">
-//                     <h1>{quote.quote}</h1>
-//                 </div>
-
-//                 <div className="Source">
-//                     <h4>
-//                         {quote.author}{" "}
-//                         <span className="Years">{quote.lifespan}</span>
-//                     </h4>
-//                 </div>
-
-//                 <div className="TextComponent Flexbox-item-2">
-//                     <button className="PlusButton">+</button>
-//                     {/* <span>{quote.votes}</span> */}
-//                     <span>0</span>
-//                     <button className="MinusButton">-</button>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
 
 export default QuoteDetail;
